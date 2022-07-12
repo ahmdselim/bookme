ae6467b9bb4bc6f59cc86ba30fa340aafe624915
khali70 <k7alifa.a7med70@gmail.com>
2022-07-12 11:43:16 +0200

    firebase .env keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -11,23 +11,8 @@ import {
   sendPasswordResetEmail,
   signOut,
 } from "firebase/auth";
-import { query, getDocs, collection, where, addDoc } from "firebase/firestore";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCtKRIMiCEGSQ_y1l5FU69exFo89PBwSDc",
-  authDomain: "bookme-4a5a2.firebaseapp.com",
-  projectId: "bookme-4a5a2",
-  storageBucket: "bookme-4a5a2.appspot.com",
-  messagingSenderId: "310335065274",
-  appId: "1:310335065274:web:1b0e191cd473b197a3c683",
-  measurementId: "G-92NFKPE4R8",
-};
-
+import { collection, addDoc } from "firebase/firestore";
+import { firebaseConfig } from './key'
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
